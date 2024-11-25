d53d3be604aa1e626b41be7c84fcab91bcc5aaba
Igor <50610898+Igor3007@users.noreply.github.com>
2024-11-25 17:19:26 +0300

    save

--- dist/js/common.js
+++ dist/js/common.js
@@ -701,10 +701,21 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
     }
 
+    /* ================================
+    faq
+    ================================*/
 
+    if (document.querySelectorAll('.faq-item__question')) {
 
+        const faqItems = document.querySelectorAll('.faq-item__question')
 
+        faqItems.forEach(function (item, index) {
+            item.addEventListener('click', function () {
+                this.parentNode.classList.toggle('open')
+            })
+        })
 
+    }
 
 
 }); //dcl

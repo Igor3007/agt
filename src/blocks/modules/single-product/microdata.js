83aae383ebf6f40041dfa836e39d4b30923cffa5
Igor <50610898+Igor3007@users.noreply.github.com>
2024-12-18 17:05:44 +0300

    save

---
+++ src/blocks/modules/single-product/microdata.js
@@ -0,0 +1,24 @@
+{
+    "@context": "https://schema.org",
+    "@type": "Product",
+    "name": "Зимний сигнальный огнестойкий анстистатический полукомбинезон Brodeks, mw18-05, желтый/черный",
+    "image": "/img/product/sp1.webp",
+    "description": "Куртка мужская летняя Brodeks KS 234 рабочая куртка для специалистов и рабочих, которым важны практичность, аккуратный внешний вид и удобство спецодежды по оптимальной цене. ",
+    "sku": "12345",
+    "brand": {
+        "@type": "Brand",
+        "name": "Brodex"
+    },
+    "offers": {
+        "@type": "Offer",
+        "url": "/img/product/sp1.webp",
+        "priceCurrency": "RUB",
+        "price": "1000.00",
+        "itemCondition": "https://schema.org/NewCondition",
+        "availability": "https://schema.org/InStock",
+        "seller": {
+            "@type": "Organization",
+            "name": "AG - Technology"
+        }
+    }
+}

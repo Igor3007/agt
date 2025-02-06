e3f38e80a4b6932242010dafd18bcae7f8d6e4f8
Igor <50610898+Igor3007@users.noreply.github.com>
2025-02-06 11:42:43 +0300

    fix cart

--- dist/js/cart.js
+++ dist/js/cart.js
@@ -2,9 +2,11 @@ class Cart {
     constructor(paraps) {
         this.$el = paraps.el
         this.selectAll = this.$el.querySelector('[data-cart="select-all"]')
+        this.removeAll = this.$el.querySelector('[data-cart="remove-selected"]')
         this.items = this.$el.querySelectorAll('.cart-item')
         this.popupConfirm = null
         this.popupContainer = null
+        this.timeout = 10000
         this.init()
     }
 
@@ -13,7 +15,7 @@ class Cart {
     }
 
     startTimer(duration, display) {
-        let timer = duration,
+        let timer = duration - 1,
             minutes, seconds;
         let instanceTimer = setInterval(() => {
 
@@ -27,51 +29,84 @@ class Cart {
 
             if (--timer < 0) {
                 clearInterval(instanceTimer)
-                display.parentNode.removeAttribute('disabled')
-                display.remove()
             }
         }, 1000);
     }
 
-    removeConfirm() {
+    removeConfirm(data) {
         return `
             <div class="remove-confirm" >
                 <div class="remove-confirm__icon" >
                     <span>Товар удален из корзины</span>
                 </div>
-                <div class="remove-confirm__name" >Кофта мужская зимняя KW 215</div>
+                <div class="remove-confirm__name" >${data.name}</div>
                 <div class="remove-confirm__recovery" >
                     <button class="btn" >Восстановить</button>
                 </div>
-                <div class="remove-confirm__time" >00:15</div>
+                <div class="remove-confirm__time" >00:${(this.timeout/1000)}</div>
             </div>
         `;
     }
 
-    removeAjax() {
+    removeAjax(items) {
+
+        let product_id = Array.from(items).map(item => item.dataset.id)
+
         // ajax запрос на удаление из корзины
+        console.log(product_id)
         console.log('Товар удален из корзины')
     }
 
     removeItem(items) {
-        this.popupConfirm = document.createElement('div')
-        this.popupConfirm.innerHTML = this.removeConfirm()
-        this.startTimer(15, this.popupConfirm.querySelector('.remove-confirm__time'))
-        this.popupContainer = document.createElement('div')
-        this.popupContainer.classList.add('confirm-container')
-        this.popupContainer.append(this.popupConfirm)
-        document.body.append(this.popupContainer)
+        let popupConfirm = document.createElement('div')
+        popupConfirm.innerHTML = this.removeConfirm({
+            name: items.length > 1 ? 'Удалено товаров: (' + items.length + ')' : items[0].querySelector('.cart-item__name').innerText
+        })
+        this.startTimer((this.timeout / 1000), popupConfirm.querySelector('.remove-confirm__time'))
 
-        // let timer = setTimeout(() => {
-        //     this.removeAjax()
-        //     this.popupConfirm.remove()
-        // }, 15000)
+        if (!this.popupContainer) {
+            this.popupContainer = document.createElement('div')
+            this.popupContainer.classList.add('confirm-container')
+            document.body.append(this.popupContainer)
+        }
 
-        // this.popupConfirm.querySelector('.btn').addEventListener('click', () => {
-        //     clearTimeout(timer)
-        //     this.popupConfirm.remove()
-        // })
+        this.popupContainer.append(popupConfirm)
 
+
+        items.forEach(item => {
+            item.classList.add('is-hide-animate')
+            setTimeout(() => {
+                item.classList.add('is-hide')
+            }, 600)
+        })
+
+        let timer = setTimeout(() => {
+            this.removeAjax(items)
+            popupConfirm.remove()
+        }, this.timeout)
+
+        popupConfirm.querySelector('.btn').addEventListener('click', () => {
+            clearTimeout(timer)
+            popupConfirm.remove()
+            items.forEach(item => {
+                item.classList.toggle('is-hide', false)
+                item.classList.toggle('is-hide-animate', false)
+            })
+        })
+
+
+    }
+
+    removeSelected() {
+        let result = [];
+
+        this.items.forEach(item => {
+            if (item.querySelector('[type="checkbox"]').checked) {
+                result.push(item)
+            }
+        })
+
+        this.removeItem(result)
 
     }
 
@@ -86,6 +121,10 @@ class Cart {
             item.querySelector('[data-cart="remove"]').addEventListener('click', () => {
                 this.removeItem([item])
             })
+        })
+
+        this.removeAll.addEventListener('click', () => {
+            this.removeSelected()
         })
     }
 }

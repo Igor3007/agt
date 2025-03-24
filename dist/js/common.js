b8721690ab2e5c03bc037fd313a3cfbd0c951e99
Igor <50610898+Igor3007@users.noreply.github.com>
2025-03-24 14:04:15 +0300

    fix page news details

--- dist/js/common.js
+++ dist/js/common.js
@@ -1118,6 +1118,92 @@ document.addEventListener('DOMContentLoaded', function (event) {
     }
 
     /* ================================
+    slider news-recomendet
+    ================================*/
+
+    if (document.querySelector('[data-slider="news-recomendet"]')) {
+
+        const items = document.querySelectorAll('[data-slider="news-recomendet"]')
+
+        items.forEach(slider => {
+
+            let splide = new Splide(slider, {
+
+                arrows: true,
+                arrowPath: SLIDER_ARROW_PATH,
+                pagination: false,
+                gap: 20,
+                autoWidth: true,
+                start: 0,
+                perPage: 1,
+                perMove: 4,
+                flickMaxPages: 1,
+                flickPower: 100,
+                breakpoints: {
+
+                    1400: {
+                        perMove: 4,
+                    },
+
+                    1200: {
+                        perMove: 3,
+                    },
+
+                    992: {
+                        perMove: 2,
+                    },
+
+                    576: {
+                        destroy: true
+                    },
+                },
+
+            });
+
+            const getTopArrowButtons = () => {
+
+                if (slider) {
+                    let heigthEl = slider.querySelector('picture').clientHeight
+                    slider.querySelectorAll('.splide__arrow').forEach(btn => {
+                        btn.style.top = (heigthEl / 2) + 'px'
+                    })
+                }
+            }
+
+            splide.on('mounted', (e) => {
+
+                if (splide.length == (splide.options.perPage)) {
+                    nextButton.setAttribute('aria-hidden', '')
+                    prevButton.setAttribute('aria-hidden', '')
+                }
+
+                //auto perMove
+                const getPerMove = () => {
+                    return Math.floor((splide.root.clientWidth / splide.root.querySelector('.splide__slide').clientWidth)) || 1
+                }
+
+                splide.options = {
+                    perMove: getPerMove(),
+                };
+
+                // top for nan button
+                getTopArrowButtons()
+
+            })
+
+            splide.on('resize', (e) => {
+                getTopArrowButtons()
+            })
+
+
+            splide.mount();
+        })
+
+
+
+    }
+
+    /* ================================
     faq
     ================================*/
 
